cb53673727a63997984b06e7cc8c103b66eaea81
ninjathewidowmaker <balummiryala4@yahoo.com>
2026-04-07 23:19:55 +0530

    Nothing crazy just trying to hit 4 updates to get brighter green stars in GitHub Contributions

--- Search-Tools/test-links.py
+++ Search-Tools/test-links.py
@@ -1,4 +1,11 @@
 import subprocess
 
+# Define the command as a list for better security and reliability
+url = "" #Enter the url
+command = ["links", url]  
 
-subprocess.run("")
+
+result = subprocess.run(command, check=True, text=True)
+    
+    
+#Alright this is day 2 of my expirement with the GitHub contributions.    
