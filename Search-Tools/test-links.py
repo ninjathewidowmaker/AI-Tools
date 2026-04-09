c73307257c097010f18cd174cabf099658660514
ninjathewidowmaker <balummiryala4@yahoo.com>
2026-04-09 12:27:37 +0530

    Converted this into a function.

--- Search-Tools/test-links.py
+++ Search-Tools/test-links.py
@@ -1,11 +1,15 @@
 import subprocess
 
-# Define the command as a list for better security and reliability
-url = "" #Enter the url
-command = ["links", url]  
+
+#Day 3 converted it into a function so that it can be used in other projects.
+def web_scrape(url):
+  command = ["links", url]
+  result = subprocess.run(command, check=True, text=True)  
+  return result
 
 
-result = subprocess.run(command, check=True, text=True)
+
     
     
-#Alright this is day 2 of my expirement with the GitHub contributions.    
+#Day 3 And made this into a function.    
+web_scrape('https://pokemon.com')
